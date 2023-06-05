b05ea5899315c8d8d53029a78040a2ec9dacd729
Sophia <sophia@MacBook-Pro-Sophia.local>
2023-06-05 21:10:00 +0300

    All

---
+++ archivator/archive.cpp
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>URL</key>
+	<string>https://github.com/is-itmo-c-22/labwork-4-Sophia199768/pull/1/commits/d60b48e417d2d807fa9bfdeeced2792566b33159#diff-e1f7135c9eaa0f1bd7d99702ddf20dab4455bc80b54be2e9de1dec902b06ce38</string>
+</dict>
+</plist>
---
+++ archivator/decoding_hamming.cpp
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>URL</key>
+	<string>https://github.com/is-itmo-c-22/labwork-4-Sophia199768/pull/1/commits/d60b48e417d2d807fa9bfdeeced2792566b33159#diff-bd1275777ee023175426046d769b07531f9d974718da566e1842eb42b0004437</string>
+</dict>
+</plist>
